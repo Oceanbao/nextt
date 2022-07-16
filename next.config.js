140c93a3543005c3fdd7077329d5e9e0a2010f15
oceanbao <oceanbao@outlook.com>
2022-07-16 14:41:38 +0800

    add: bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,14 +1,18 @@
 // @ts-check
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 /**
  * @type {import('next').NextConfig}
  **/
-const nextConfig = {
+const nextConfig = withBundleAnalyzer({
   reactStrictMode: true,
   // Styled Component upgrade to SWC compiler
   // ssr and displayName are configured by default
   styledComponents: true,
   swcMiniy: true,
-}
+})
 
 module.exports = nextConfig
